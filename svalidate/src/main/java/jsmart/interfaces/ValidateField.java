5c3dd716c39ef5e44881cf31d4269092a2e6c02b
InSunShine2015 <3284933048@qq.com>
2017-01-16 16:39:52 +0800

    添加注释

--- svalidate/src/main/java/jsmart/interfaces/ValidateField.java
+++ svalidate/src/main/java/jsmart/interfaces/ValidateField.java
@@ -2,7 +2,18 @@ package jsmart.interfaces;
 
 import java.util.Map;
 import jsmart.entity.ValidateMsg;
-
+/**
+ * 验证实体类的字段
+ * @author sUN
+ *
+ */
 public interface ValidateField {
+	/**
+	 * 
+	 * @param fieldName 属性名称
+	 * @param values 验证需要的值
+	 * @param msg 验证结果
+	 * @return
+	 */
 	ValidateMsg validate(String fieldName,Map<String,Object> values,ValidateMsg msg);
 }
